d5d9e1dc52c6ede63c4db546953d9d4dacaf4a28
Ponts <pbrink@kth.se>
2018-02-04 02:50:28 +0100

    Kinematic car LSR

--- Source/Ass1/Private/NodeSelector.cpp
+++ Source/Ass1/Private/NodeSelector.cpp
@@ -3,22 +3,25 @@
 #include "NodeSelector.h"
 
 
+
 NodeSelector::NodeSelector()
 {
 	XBound = 30.f;
 	YBound = 30.f;
 	PathSize = 3;
-	NumNodes = 1000;
-	GoalRadius = 1.f;
+	NumNodes = 3000;
+	GoalRadius = 0.3f;
 	nodes = TArray<Node*>();
+	CarNodes = TArray<CarNode*>();
 	DynamicNodes = TArray<DynamicNode*>();
 	obstacles = TArray<Obstacle>();
 	StepSize = 20;
 	TimeStep = 0.1;
-	VehicleLength = 2;
+	VehicleLength = 2.0;
 	Velocity = 1.0;
 	MaxTurnSpeed = 1.0;
 	Acceleration = 1.3;
+	MaxTurnAngle = 0.6;
 	GoalVelocity = FVector(0.5f, -0.5f, 0.0f);
 }
 
@@ -32,6 +35,19 @@ void NodeSelector::GetRrtPath(TArray<Node*>& vectors) {
 	//nodes[0] is startposition so shouldnt include it
 	Node* CurrentNode = nodes[nodes.Num() - 1];
 	while (CurrentNode->point != nodes[0]->point) {
+		CurrentNode->point.Z = 0;
+		vectors.Add(CurrentNode);
+		CurrentNode = CurrentNode->parent;
+	}
+	UE_LOG(LogTemp, Display, TEXT("TIME: %f"), vectors.Num()*TimeStep);
+	Algo::Reverse(vectors);
+}
+
+void NodeSelector::getCarRrtPath(TArray<CarNode*>& vectors) {
+	vectors.Empty();
+	//nodes[0] is startposition so shouldnt include it
+	CarNode* CurrentNode = CarNodes[CarNodes.Num() - 1];
+	while (CurrentNode->point != CarNodes[0]->point) {
 		CurrentNode->point.Z = 0;
 		vectors.Add(CurrentNode);
 		CurrentNode = CurrentNode->parent;
@@ -270,18 +286,18 @@ DynamicNode* NodeSelector::CalculateDynamicPointNode(const DynamicNode& n1, FVec
 	FVector direction = n2 - n1.point;
 	//Make acceleration towards direction
 	direction /= direction.Size(); //Normalize;
-	direction *= Acceleration;     //Set to max acceleration
-	if (currentVelocity.Size() > Velocity) {
-		currentVelocity /= currentVelocity.Size(); //Normalize
-		currentVelocity *= Velocity;			//Set to max velocity
-		//UE_LOG(LogTemp, Display, TEXT("New Velocity: %f"), currentVelocity.Size());
-	}
+	direction *= Acceleration * TimeStep;     //Set to max acceleration
 	FVector NewVelocity = currentVelocity + direction;
-	NewVelocity = NewVelocity * Velocity / NewVelocity.Size(); //Set length to velocity
+	//NewVelocity = NewVelocity * Velocity / NewVelocity.Size(); //Set length to velocity
+	if (NewVelocity.Size() > Velocity) {
+		NewVelocity /= currentVelocity.Size(); //Normalize
+		NewVelocity *= Velocity;			//Set to max velocity
+												//UE_LOG(LogTemp, Display, TEXT("New Velocity: %f"), currentVelocity.Size());
+	}
 	FVector NewPosition = n1.point + NewVelocity * TimeStep;
-
 	return new DynamicNode(n1,NewPosition,NewVelocity);
 }
+
 
 void NodeSelector::dynamicPointRrt(FVector EndPosition, FVector StartPosition, FVector StartVelocity, FVector EndVelocity) {
 	DynamicNodes.Empty();
@@ -333,3 +349,220 @@ void NodeSelector::dynamicPointRrt(FVector EndPosition, FVector StartPosition, F
 }
 
 
+//-------------------------------------------------------//
+//Kinematic Car RRT
+//-------------------------------------------------------//
+
+CarNode* NodeSelector::CalculateCarNode(const CarNode&n1, const FVector n2) {
+	FVector path = n2 - n1.point;
+	float angle = acos(GetCosAngle(n1.orientation, path));
+	float orientation = acos(GetCosAngle(FVector(1, 0, 0), n1.orientation));
+	if (n1.orientation.Y < 0)
+		orientation = -orientation;
+	float turn = 0.0f;
+	if (angle > 0.00001) {
+		FVector cross = FVector::CrossProduct(path, n1.orientation);
+		float partTurn = (Velocity / VehicleLength)*tan(MaxTurnAngle)*TimeStep / angle;
+		if (partTurn < 1.f)
+			turn = MaxTurnAngle * copysign(1.0, -cross.Z);
+		else
+			turn = atan(angle*VehicleLength / Velocity)*copysign(1.0, -cross.Z);
+	}
+	float delta = (Velocity / VehicleLength)*tan(turn)*TimeStep;
+	orientation = delta + orientation;
+	FVector newOrientation = FVector(cos(orientation), sin(orientation), n1.point.Z);
+	float newX = Velocity * cos(orientation)*TimeStep;
+	float newY = Velocity * sin(orientation)*TimeStep;
+	FVector newPosition = FVector(n1.point.X + newX, n1.point.Y + newY, n1.point.Z);
+	return new CarNode(n1, newPosition, newOrientation);
+}
+
+std::vector<std::pair<FVector,FVector>> NodeSelector::TangentLines(FVector c1, FVector c2, float radc1, float radc2) {
+	float d_sq = pow(c2.X-c1.X,2)+pow(c2.Y-c1.Y,2);
+	std::vector<std::pair<FVector, FVector>> returnVec;
+	if (d_sq < (radc1 - radc2)*(radc1 - radc2)) {
+		//we may have a problem, the circles are either intersecting, one is within the other, but still tangent
+		//at one point, or one is completely in the other. We only have a problem if one is within the other, but
+		//not tangent to it anywhere
+		if (d_sq != std::fmaxf(radc1, radc2) && d_sq < std::fmaxf(radc1, radc2)){
+			return returnVec;
+		}//else they are intersecting or one is within the other, but still tangent to it
+		 //in the other two cases, either 1 or 2 external tangent lines remain, but there are no internal tangent
+		 //lines
+	}
+
+	float d = sqrtf(d_sq);
+	float vx = (c2.X - c1.X) / d;
+	float vy = (c2.Y - c1.Y) / d;
+	for (int sign1 = +1; sign1 >= -1; sign1 -= 2) {
+		float c = (radc1 - sign1 * radc2) / d;
+		if (c*c > 1.0) continue; //want to be subtracting small from large, not adding
+		double h = sqrt(std::fmaxf(0.0, 1.0 - c * c));
+
+		for (int sign2 = +1; sign2 >= -1; sign2 -= 2) {
+			float nx = vx * c - sign2 * h * vy;
+			float ny = vy * c + sign2 * h * vx;
+			returnVec.push_back(std::make_pair((FVector(c1.X + radc1 * nx, c1.Y + radc1 * ny,0.0f)),
+				(FVector(c2.X + sign1 * radc2 * nx, c2.Y + sign1 * radc2 * ny,0.0f))));
+		}
+	}
+	
+	return std::vector<std::pair<FVector, FVector>>();
+}
+
+float NodeSelector::ArcLength(FVector center, FVector left, FVector right, float radius, bool isleft) {
+	FVector vec1, vec2;
+	vec1 = left - center;
+	vec2 = right - center;
+	float theta = atan2(vec2.Y, vec2.X) - atan2(vec1.Y, vec1.X);
+	if (theta < -1e-6 && isleft)
+		theta += 2.0*PI;
+	else if (theta > 1e-6 && !isleft)
+		theta -= 2.0 * PI;
+	return fabs(theta*radius);
+}
+
+TArray<CarNode*> NodeSelector::LSR(std::vector<std::pair<FVector, FVector>>& _LRTangents, const FVector agentleft, float radleft, const FVector goalright, float radright, FVector currPos,FVector goalPos,const CarNode&n1) {
+	TArray<CarNode*> next;
+	float arcL1, arcL2, arcL3;
+	if (_LRTangents.size() > 3) {
+		//CALCULATE LEFT CIRCLE
+		arcL1 = ArcLength(agentleft, currPos, _LRTangents[3].first, radleft, true);
+		float timesteps = arcL1 / (Velocity * TimeStep);
+		float delta = (Velocity / VehicleLength)*tan(MaxTurnAngle)*TimeStep;
+		float orientation = acos(GetCosAngle(FVector(1, 0, 0), n1.orientation));
+		orientation = delta + orientation;
+		FVector newOrientation = FVector(cos(orientation), sin(orientation), n1.point.Z);
+		float newX = Velocity * cos(orientation)*TimeStep;
+		float newY = Velocity * sin(orientation)*TimeStep;
+		FVector newPosition = FVector(n1.point.X + newX, n1.point.Y + newY, n1.point.Z);
+		next.Add(new CarNode(n1, newPosition, newOrientation));
+		for (int i = 1; i < timesteps; ++i) {
+			orientation = acos(GetCosAngle(FVector(1, 0, 0), next[i-1]->orientation));
+			orientation = delta + orientation; //THIS MIGHT BE TURN RIGHT
+			newOrientation = FVector(cos(orientation), sin(orientation), next[i - 1]->point.Z);
+			newX = Velocity * cos(orientation)*TimeStep;
+			newY = Velocity * sin(orientation)*TimeStep;
+			newPosition = FVector(next[i - 1]->point.X + newX, next[i - 1]->point.Y + newY, next[i - 1]->point.Z);
+			next.Add(new CarNode(next[i - 1], newPosition, newOrientation));	
+		}
+		//CALCULATE STRAIGHT LINE
+		arcL2 = (_LRTangents[3].first - _LRTangents[3].second).Size();
+		timesteps = arcL2 / (Velocity * TimeStep);
+		newX = Velocity * cos(orientation)*TimeStep;
+		newY = Velocity * sin(orientation)*TimeStep;
+		newPosition = FVector(next[next.Num()-1]->point.X + newX, next[next.Num() - 1]->point.Y + newY, next[next.Num() - 1]->point.Z);
+		next.Add(new CarNode(n1, newPosition, newOrientation));
+		for (int i = 1; i < timesteps; ++i) {
+			newX = Velocity * cos(orientation)*TimeStep;
+			newY = Velocity * sin(orientation)*TimeStep;
+			newPosition = FVector(next[next.Num() - 1]->point.X + newX, next[next.Num() - 1]->point.Y + newY, next[next.Num() - 1]->point.Z);
+			next.Add(new CarNode(next[next.Num() - 1], newPosition, newOrientation));
+		}
+		//CALCULATE RIGHT CIRCLE
+		arcL3 = ArcLength(goalright, _LRTangents[3].second,goalPos, radleft, false);
+		timesteps = arcL3 / (Velocity * TimeStep);
+		delta = (Velocity / VehicleLength)*tan(MaxTurnAngle)*TimeStep;
+		orientation = acos(GetCosAngle(FVector(1, 0, 0), next[next.Num() - 1]->orientation));
+		orientation = delta + orientation;
+		newOrientation = FVector(cos(orientation), sin(orientation), next[next.Num() - 1]->point.Z);
+		newX = Velocity * cos(orientation)*TimeStep;
+		newY = Velocity * sin(orientation)*TimeStep;
+		newPosition = FVector(next[next.Num() - 1]->point.X + newX, next[next.Num() - 1]->point.Y + newY, next[next.Num() - 1]->point.Z);
+		next.Add(new CarNode(n1, newPosition, newOrientation));
+		for (int i = 1; i < timesteps; ++i) {
+			orientation = acos(GetCosAngle(FVector(1, 0, 0), next[next.Num() - 1]->orientation));
+			orientation = delta - orientation; //THIS MIGHT BE TURN RIGHT
+			newOrientation = FVector(cos(orientation), sin(orientation), next[next.Num() - 1]->point.Z);
+			newX = Velocity * cos(orientation)*TimeStep;
+			newY = Velocity * sin(orientation)*TimeStep;
+			newPosition = FVector(next[next.Num() - 1]->point.X + newX, next[next.Num() - 1]->point.Y + newY, next[next.Num() - 1]->point.Z);
+			next.Add(new CarNode(next[next.Num() - 1], newPosition, newOrientation));
+		}
+	}
+	return next;
+}
+
+void NodeSelector::CalculateTangentPoints(CarNode& n1, CarNode& n2) {
+	float r1 = VehicleLength / tan(MaxTurnAngle);
+	float r2 = r1;
+	FVector cross = FVector::CrossProduct(n1.orientation, FVector(0, 0, 1));
+	FVector fromLeft = n1.point + ((cross / cross.Size()))*r1; //Left Circle at current position
+	FVector fromRight = n1.point - ((cross / cross.Size()))*r1;
+	cross = FVector::CrossProduct(n2.orientation, FVector(0, 0, 1));
+	FVector toLeft = n2.point + ((cross / cross.Size()))*r2;
+	FVector toRight = n2.point - ((cross / cross.Size()))*r2;
+	TArray<CarNode*> next;
+	std::vector<std::pair<FVector, FVector>> _LSRTangents = TangentLines(fromLeft, toRight, r1,r2);
+	next = LSR(_LSRTangents, fromLeft, r1, toRight, r2, n1.point, n2.point, n1);
+	// Check if collides, check if current orientation is correct get smallest path of those.
+	for (int i = 0; i < next.Num(); ++i) {
+		//check collides
+	}
+
+	
+
+
+}
+
+CarNode* NodeSelector::GetDubinsPath(const CarNode&n1, const CarNode&n2) {
+	float r1 = VehicleLength/tan(MaxTurnAngle);
+	float r2 = r1;
+	
+	
+	
+	
+
+
+	return new CarNode(n1);
+}
+
+
+void NodeSelector::carRrt(FVector EndPosition, FVector StartPosition, FVector StartOrientation, FVector EndOrientation) {
+	CarNodes.Empty();
+	//Create startnode
+	CarNode* StartNode = new CarNode(StartPosition, StartOrientation);
+	CarNodes.Add(StartNode);
+	int count = 0;
+	bool foundNext = false;
+	float x = 0;
+	float y = 0;
+	FVector rand = FVector(x, y, StartPosition.Z);
+	CarNode* parent = CarNodes[0];
+	CarNode* NewNode = parent;
+	while (count < NumNodes) {
+		//Check if there is a straight line to the target from the current position
+		foundNext = false;
+		while (!foundNext) {
+			rand.X = FMath::RandRange(-XBound, XBound);
+			rand.Y = FMath::RandRange(-YBound, YBound);
+			if (count % 20 == 0) {
+				rand.X = EndPosition.X;
+				rand.Y = EndPosition.Y;
+
+				//UE_LOG(LogTemp, Display, TEXT("Sampling goal node"));
+			}
+			for (int i = 0; i < CarNodes.Num(); ++i) {
+				if (PointDistance(CarNodes[i]->point, rand) <= PointDistance(parent->point, rand)) {
+					NewNode = CalculateCarNode(*CarNodes[i], rand);
+					parent = CarNodes[i];
+					foundNext = true;
+				}
+			}
+		}
+		//NewNode.Z = 70.f;
+		//UE_LOG(LogTemp, Display, TEXT("%f, %f"), NewNode.X, NewNode.Y);
+
+		CarNodes.Add(new CarNode(parent, NewNode->point, NewNode->orientation));
+
+		if (PointDistance(NewNode->point, EndPosition)<GoalRadius) {
+			CarNodes.Add(new CarNode(parent, EndPosition, EndOrientation));
+			UE_LOG(LogTemp, Display, TEXT("FOUND GOAL"));
+			count = NumNodes;
+		}
+		//Draw debug line
+		count++;
+	}
+	UE_LOG(LogTemp, Display, TEXT("nr of nodes: %d"), CarNodes.Num());
+	//UE_LOG(LogTemp, Display, TEXT("Node 2 point: %f, %f"), DynamicNodes[2]->point.X, DynamicNodes[2]->point.Y);
+}
---
+++ Source/Ass1/Public/KinematicCar.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NodeSelector.h"
+#include "GameFramework/Pawn.h"
+#include "KinematicPointMovementComponent.h"
+#include "DrawDebugHelpers.h"
+#include "CarNode.h"
+#include "KinematicCar.generated.h"
+/**/
+UCLASS()
+class ASS1_API AKinematicCar : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	class UKinematicPointMovementComponent* MovementComponent;
+	class USpringArmComponent* SpringArm;
+	bool HasGoalPosition = false;
+	FVector GoalPosition;
+	NodeSelector NodeSelector;
+	TArray<CarNode*> path;
+	AKinematicCar();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	virtual UPawnMovementComponent* GetMovementComponent() const override;
+
+	virtual void MoveForward(float AxisValue);
+	virtual void MoveRight(float AxisValue);
+	virtual void Turn(float AxisValue);
+	virtual void RandomTurn();
+	virtual void RandomPosition();
+	virtual void MoveToPosition(float x, float y);
+	virtual void GetPath();
+	virtual void DrawDebugLines();
+	virtual void DrawGraph();
+	float CalculateDistanceToGoal(const FVector);
+	
+};
